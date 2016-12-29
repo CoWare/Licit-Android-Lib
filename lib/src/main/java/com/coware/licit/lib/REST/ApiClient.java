aa857705730864abf7003477b6f73030f608bb5f
bolorundurowb <ogatimo@gmail.com>
2016-12-29 16:31:03 +0100

    feature: adds rest client and request logging

---
+++ lib/src/main/java/com/coware/licit/lib/REST/ApiClient.java
@@ -0,0 +1,38 @@
+package com.coware.licit.lib.REST;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by bolorundurowb on 12/29/16.
+ */
+
+public class ApiClient {
+    private static final String API_URL = "http://licit.com/api";
+    private static Licit REST_CLIENT;
+
+    static {
+        setupRestClient();
+    }
+
+    public static Licit getRestClient() {
+        return REST_CLIENT;
+    }
+
+    private static void setupRestClient() {
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        httpClient.addInterceptor(logging);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(API_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient.build())
+                .build();
+
+        REST_CLIENT = retrofit.create(Licit.class);
+    }
+}
---
+++ lib/src/main/java/com/coware/licit/lib/REST/Licit.java
@@ -0,0 +1,8 @@
+package com.coware.licit.lib.REST;
+
+/**
+ * Created by bolorundurowb on 12/29/16.
+ */
+
+public interface Licit {
+}
